bb2916ca6d1b7a9cef65ae0f12aaecc7359ece8a
Jsharkc <1773867204@qq.com>
2017-09-01 11:43:06 +0800

    完成基本游戏逻辑 #3

--- component/component.go
+++ component/component.go
@@ -2,6 +2,15 @@ package component
 
 import (
 	"fmt"
+
+	"github.com/nsf/termbox-go"
+)
+
+const (
+	introduce = `
+	说明：按括号里的字母选择人物，上下左右移动，(c) 重新开始游戏.
+	`
+	stepStr = "step:%d"
 )
 
 var (
@@ -12,15 +21,81 @@ var (
 	// PosiMap Position is exist
 	PosiMap map[int8]bool
 
-	initPosiDate = []int8{2, 3, 7, 8, 1, 6, 4, 9, 11, 16, 14, 19, 12, 13, 21, 17, 18, 24, 22, 23}
+	baseX    = 10
+	baseY    = 5
+	baseGapX = 13
+	baseGapY = 2
+	baseDist = 25
+
+	tipX    = 0
+	tipY    = 2
+	tipDist = 8
+
+	// Step game step
+	Step     = 0
+	stepX    = 0
+	stepY    = 3
+	stepDist = 10
+
+	introX = 2
+	introY = 5
+
+	initPosiDate = []int8{12, 13, 17, 18, 1, 6, 4, 9, 11, 16, 14, 19, 2, 3, 21, 7, 8, 24, 22, 23}
 	initPosiSize = []int8{4, 2, 2, 2, 2, 2, 1, 1, 1, 1}
-	initNameDate = []string{"曹操(q)", "张飞(w)", "黄忠(e)", "赵云(r)", "马超(t)", "关羽(y)", "小兵1(u)", "小兵2(i)", "小兵3(o)", "小兵4(p)"}
+	initNameDate = []string{"曹操(q)", "张飞(w)", "黄忠(e)", "赵云(r)", "马超(t)", "关羽(y)", "小兵(u)", "小兵(i)", "小兵(o)", "小兵(p)"}
+
+	// COLORS colors
+	COLORS = []termbox.Attribute{
+		termbox.ColorRed,
+		termbox.ColorYellow,
+		termbox.ColorWhite,
+		termbox.ColorGreen,
+		termbox.ColorBlue,
+		termbox.ColorMagenta,
+		termbox.ColorCyan,
+	}
 )
 
 // Component is rectangle
 type Component struct {
 	Name     string
+	Size     int8
 	Position []int8
+	BgColor  termbox.Attribute
+}
+
+// Draw himself
+func (c *Component) Draw() {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+	for _, posi := range c.Position {
+		x := baseX + (int(posi)%5-1)*baseGapX
+		y := baseY + int(posi)/5*baseGapY
+
+		termbox.SetCell(x, y, ' ', termbox.ColorBlack, c.BgColor)
+		termbox.Flush()
+		fmt.Printf("%s", c.Name)
+	}
+
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
+}
+
+// Clear himself
+func (c *Component) Clear() {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+	for _, posi := range c.Position {
+		x := baseX + (int(posi)%5-1)*13
+		y := baseY + int(posi)/5*2
+
+		termbox.SetCell(x, y, ' ', termbox.ColorBlack, termbox.ColorDefault)
+		termbox.Flush()
+		fmt.Print("       ")
+	}
+
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
 }
 
 // Move a gap
@@ -41,7 +116,6 @@ func (c *Component) Move(gap int8) bool {
 				canMoveFlag = false
 				break
 			}
-
 			continue
 		}
 
@@ -50,6 +124,8 @@ func (c *Component) Move(gap int8) bool {
 	}
 
 	if canMoveFlag {
+		Step++
+		c.Clear()
 		for index, posi := range c.Position {
 			c.Position[index] = posi + gap
 			PosiMap[posi+gap] = true
@@ -61,12 +137,10 @@ func (c *Component) Move(gap int8) bool {
 
 		if winFlag == 2 {
 			Tip = "You are win!"
-
 			return true
 		}
 
-		Tip = "Had move"
-
+		Tip = "Had move    "
 		return false
 	}
 
@@ -75,9 +149,42 @@ func (c *Component) Move(gap int8) bool {
 		PosiMap[posi+gap] = true
 	}
 
-	Tip = "Don't move"
+	Tip = "Don't move  "
 
 	return false
+}
+
+// DrawTip draw tip
+func DrawTip() {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.SetCell(tipX, tipY, ' ', termbox.ColorBlack, COLORS[2])
+
+	termbox.Flush()
+	fmt.Printf("%s", Tip)
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
+}
+
+// DrawStep draw step
+func DrawStep() {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.SetCell(stepX, stepY, ' ', COLORS[2], termbox.ColorDefault)
+
+	termbox.Flush()
+	fmt.Printf(stepStr, Step)
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
+}
+
+// DrawIntroduce draw introduce
+func DrawIntroduce() {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.SetCell(introX, introY+5*baseGapY, ' ', termbox.ColorWhite, termbox.ColorDefault)
+
+	termbox.Flush()
+	fmt.Printf("%s", introduce)
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
 }
 
 // InitCompMap initial
@@ -91,6 +198,11 @@ func InitCompMap(file ...string) {
 
 	CompMap = make(map[int]*Component)
 	PosiMap = make(map[int8]bool)
+	tempX, _ := termbox.Size()
+
+	tipX = tempX/2 - tipDist
+	baseX = tempX/2 - baseDist
+	stepX = tempX/2 + stepDist
 
 	if len(file) == 0 {
 		posiDate = initPosiDate
@@ -98,13 +210,22 @@ func InitCompMap(file ...string) {
 		posiDate = readDateFrom(file[0])
 	}
 
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
 	for ; compIndex < 10; compIndex++ {
 		comp := Component{
 			Name:     initNameDate[compIndex],
+			Size:     initPosiSize[compIndex],
 			Position: make([]int8, initPosiSize[compIndex]),
 		}
 
-		totalSize += initPosiSize[compIndex]
+		if compIndex < 6 {
+			comp.BgColor = COLORS[compIndex]
+		} else {
+			comp.BgColor = COLORS[6]
+		}
+
+		totalSize += comp.Size
 		tempIndex := 0
 
 		for ; posiIndex < totalSize; posiIndex++ {
@@ -113,7 +234,11 @@ func InitCompMap(file ...string) {
 		}
 
 		CompMap[compIndex] = &comp
+		comp.Draw()
 	}
+
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
 
 	for i, posi := range posiDate {
 		if i < 18 {
@@ -123,7 +248,7 @@ func InitCompMap(file ...string) {
 		}
 	}
 
-	fmt.Printf("[DEBUG]:CompMap %v\nPosiMap %v\n", CompMap, PosiMap)
+	// fmt.Printf("\n[DEBUG]:CompMap %v\nPosiMap %v\n", CompMap, PosiMap)
 }
 
 func readDateFrom(file string) []int8 {
--- main.go
+++ main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	comp "Klotski/component"
 	"time"
 
 	"github.com/nsf/termbox-go"
+
+	comp "Klotski/component"
 )
 
 var (
-	COLORS = []termbox.Attribute{
-		termbox.ColorWhite,
-		termbox.ColorGreen,
-		termbox.ColorBlue,
-		termbox.ColorCyan,
-		termbox.ColorMagenta,
-		termbox.ColorRed,
-	}
+	curComp *comp.Component
+	isWin   bool
 )
 
-func main() {
-	comp.InitCompMap()
+// DrawAll draw game UI
+func DrawAll() {
+	for _, value := range comp.CompMap {
+		value.Draw()
+	}
+	comp.DrawTip()
+	comp.DrawStep()
+}
 
+// ClearAll clear game UI
+func ClearAll() {
+	for _, value := range comp.CompMap {
+		value.Clear()
+	}
+	comp.DrawTip()
+}
+
+func main() {
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
@@ -36,11 +46,15 @@ func main() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	termbox.Flush()
 
+	comp.InitCompMap()
+	comp.DrawIntroduce()
+	curComp = comp.CompMap[0]
+
 	// Event queue
-	event_queue := make(chan termbox.Event)
+	eventQueue := make(chan termbox.Event)
 	go func() {
 		for {
-			event_queue <- termbox.PollEvent()
+			eventQueue <- termbox.PollEvent()
 		}
 	}()
 
@@ -48,7 +62,7 @@ func main() {
 loop:
 	for {
 		select {
-		case ev := <-event_queue:
+		case ev := <-eventQueue:
 			switch ev.Type {
 			case termbox.EventKey:
 				// Exit
@@ -57,25 +71,53 @@ loop:
 				}
 
 				// Restart game (if user has already lost)
-				if ev.Ch == 'r' || ev.Ch == 'R' {
 
+				switch ev.Ch {
+				case 'q', 'Q':
+					curComp = comp.CompMap[0]
+				case 'w', 'W':
+					curComp = comp.CompMap[1]
+				case 'e', 'E':
+					curComp = comp.CompMap[2]
+				case 'r', 'R':
+					curComp = comp.CompMap[3]
+				case 't', 'T':
+					curComp = comp.CompMap[4]
+				case 'y', 'Y':
+					curComp = comp.CompMap[5]
+				case 'u', 'U':
+					curComp = comp.CompMap[6]
+				case 'i', 'I':
+					curComp = comp.CompMap[7]
+				case 'o', 'O':
+					curComp = comp.CompMap[8]
+				case 'p', 'P':
+					curComp = comp.CompMap[9]
+				case 'c', 'C':
+					ClearAll()
+					comp.InitCompMap()
+					isWin = false
+					curComp = comp.CompMap[0]
+				default:
 				}
 
-				switch ev.Key {
-				case termbox.KeyArrowLeft:
-					comp.CompMap[0].Move(1)
-				case termbox.KeyArrowRight:
-					comp.CompMap[0].Move(1)
-				case termbox.KeyArrowUp:
-					comp.CompMap[0].Move(1)
-				case termbox.KeyArrowDown:
-					comp.CompMap[0].Move(1)
+				if !isWin {
+					switch ev.Key {
+					case termbox.KeyArrowLeft:
+						isWin = curComp.Move(-1)
+					case termbox.KeyArrowRight:
+						isWin = curComp.Move(1)
+					case termbox.KeyArrowUp:
+						isWin = curComp.Move(-5)
+					case termbox.KeyArrowDown:
+						isWin = curComp.Move(5)
+					}
+
+					DrawAll()
 				}
 
 			case termbox.EventResize:
-
 			}
-
 		default:
 			time.Sleep(10 * time.Millisecond)
 		}
